b2366bbf8525865443f236c168454559b7a0c0cf
gomi_ningen <gomi2ngen@gmail.com>
2019-01-11 23:14:28 +0900

    サムネイルの遅延読み込みを実装

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,29 @@
-// This is where it all goes :)
+var allImagesLoaded = false;
+var loadImages = () => {
+  const imgs = document.querySelectorAll('img[data-src]');
+  if (imgs.length == 0) {
+    allImagesLoaded = true;
+    return;
+  }
+  const offset = window.innerHeight * 0.5;
+  const maxY = window.pageYOffset + window.innerHeight;
+  [].forEach.call(imgs, img => {
+    if (maxY >= img.offsetTop - offset) {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.onload = function() {
+        img.removeAttribute('data-src');
+      };
+    }
+  });
+}
+
+window.addEventListener('scroll', (e) => {
+
+  if (window.pageYOffset % 5 == 0 && !allImagesLoaded) {
+    loadImages();
+  }
+});
+
+window.onload = () => {
+  loadImages();
+};
